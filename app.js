ca6f45aa29ebf8cb407a1e24772bedb128162de2
Sean Naber <seannaber@gmail.com>
2017-05-01 20:51:11 -0400

    isolated routes into own directory -- isolation of concerns

--- app.js
+++ app.js
@@ -1,13 +1,21 @@
-require('./instantHello');
-var goodbye = require('./talk/goodbye');
-var talk = require('./talk');
-var question = require('./talk/question')
+var express = require('express');
+var app = express();
+var path = require('path');
 
-talk.intro();
-talk.hello('Becca');
+var routes = require('./routes');
 
-goodbye();
+app.set('port', 3000);
 
-var answer = question.ask("What is the meaning of life?");
-console.log(answer);
+app.use(function(req, res, next) {
+	console.log(req.method, req.url);
+	next();
+});
 
+app.use(express.static(path.join(__dirname, 'public')));
+
+app.use('/api', routes);
+
+var server = app.listen(app.get('port'),  function() {
+	var port = server.address().port;
+	console.log('Magic happens on port ' + port);
+});
